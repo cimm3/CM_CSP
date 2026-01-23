02258dd39810e6336bf7b63931343ceee5d98da0
cimm3 <constanza.molina@ucas-edu.net>
2026-01-23 11:44:40 -0700

    number notes

---
+++ Notes/intergers_flotas.py
@@ -0,0 +1,14 @@
+price = float (input("What is the cost of the item: $"))
+
+tax_rate= float(input("What is the tax percent in your state: %"))
+
+tax_rate_decimal = tax_rate/100
+
+total = round(price*(1 + tax_rate_decimal),2)
+print(total)300
+
+"""apples = 27
+
+friends = 4
+
+print("each of my friends can have", int(apples)//friends, "and I will have" , int(apples)%friends, "left over.")"""
