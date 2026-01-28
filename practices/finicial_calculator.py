2e31aaca8d526c3568df69d594ce42cfcdab80eb
cimm3 <constanza.molina@ucas-edu.net>
2026-01-28 02:46:45 +0000

    finicial calculator

--- practices/finicial_calculator.py
+++ practices/finicial_calculator.py
@@ -4,16 +4,24 @@ monthly_income = float (input( "How much money you earn monthly:$ " ))
 
 rent = float (input( "how much is your rent:$ "))
 
-print("You spent $", rent, "which is", ((rent/monthly_income) * 100), "% of you icome.") 
+print("You spent $", rent, "which is", round((rent/monthly_income) * 100), "% of you icome.") 
 
-monhtly_utilities = float (input ( "How much money do you spend on utilities: $ "))
+monthly_utilities = float (input ( "How much money do you spend on utilities: $ "))
 
-print("that is a totola of $", monhtly_utilities, " which is", ((monhtly_utilities/monhtly_utilities)*100), "% of your income.")
+print("that is a totola of $", monthly_utilities, " which is", round((monthly_utilities/monthly_income)*100), "% of your income.")
 
 groceries = float (input ("How much are usually your monthly groceries:$ "))
 
-print( "You spent", groceries, "that is", ((groceries/monthly_income)*100), "% of your income.")
+print( "You spent", groceries, "that is", round((groceries/monthly_income)*100), "% of your income.")
 
 transportation = float (input ( "How much do you spend on transprtation monthly: $ "))
 
-print("You spent", transportation, "that is", ((transportation/monthly_income)*100), "% of your income.")
+print("You spent", transportation, "that is", round((transportation/monthly_income)*100), "% of your income.")
+
+save = (monthly_income*0.1)
+
+print("You should save",save, "which is 10% of your income.")
+
+spending = (monthly_income- (rent+monthly_utilities+transportation+groceries+save))
+
+print("You have a total of $", spending,"spending every month!")
