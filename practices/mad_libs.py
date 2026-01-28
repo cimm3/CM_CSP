f1afb43ce6f089d59c85164315afa0e68fd46a12
cimm3 <constanza.molina@ucas-edu.net>
2026-01-28 11:43:13 -0700

    mad_lips

---
+++ practices/mad_libs.py
@@ -0,0 +1,33 @@
+# CM, mad libs python
+
+adjective = input("Tell me a adjective: ").strip()
+
+nationality = input("Give me a nationality: ").capitalize()
+
+name = input("Give me a name: ").capitalize()
+
+noun = input("GIve me a noun: ")
+
+adjective2 = input("Give me an adjective: ")
+
+noun2 = input("Give me a noun: ")
+
+adjective3 = input("Give me an adjective: ")
+
+adjective4 = input("Give me an adjective: ")
+
+plural_noun = input("Give me a plural noun: ")
+
+noun3 = input("Give me a noun: ")
+
+number = input("Give me a number: ")
+
+shape = input("Give me a shape: ")
+
+food = input("Give me a food: ")
+
+favorite_food = input("Give me a food: ")
+
+number2 = input("Give me a number: ")
+
+print("Pizza was invented by a "+adjective+" "+nationality + "chef named "+name+". To make pizza, you need to take a lump of " + noun+ ", and make a thin, round "+adjective2+ " "+ noun2+ ". Then you cover it with "+adjective3+" sauce, "+adjective4+" cheese, and fresh chopped "+plural_noun+". Next you have to bake it in a very hot "+noun3+". When it is done, cut it into "+shape+". Some kids like "+food+" pizza the best, but my favorite is the "+ favorite_food+". If I could, I would eat "+number2+" times a day!")
