495187b0bc2c067c4f35950b6c3272e8d734cd27
cimm3 <constanza.molina@ucas-edu.net>
2026-02-03 11:37:24 -0700

    fizzbuzz

---
+++ practices/fizzbuzz.py
@@ -0,0 +1,14 @@
+# CM Fizzbuz python 
+
+i = 0
+
+while i < 50:
+    i += 1
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
