f1b14541b1222df679ab77cdd640b871256236e6
cimm3 <constanza.molina@ucas-edu.net>
2026-02-19 11:34:48 -0700

    first program c

---
+++ C/notes/practices/firstprogram.c
@@ -0,0 +1,10 @@
+// CM P3, first progam C
+
+#include <stdio.h>
+
+int main(){
+    char name[50];
+    printf("What is your name: ");
+    scanf("%s", name);
+    printf("Hello %s!! This is your first program in C!\n", name);
+}
