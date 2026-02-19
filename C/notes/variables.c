6d3fa8b409cbe39bca8fc825d7c7a89e95856409
cimm3 <constanza.molina@ucas-edu.net>
2026-02-19 11:25:12 -0700

    variables C

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -4,8 +4,23 @@
 int main(){
     int number = 12;
     float pi = 3.14;
+    char name[] = "Xavier";
+    char person[50];
+    int age;
+
+     printf("Tell me your name\n");
+    scanf("%s", person);
+    printf("Tell me your name\n");
+
+    printf("tell me how old you are\n");
+    scanf("%d", &age);
+
+
+
+    printf("Name is %s and age is %d\n", person, age);
 
     printf("%d", number);
     printf("%f\n", pi); // \n tells computer to start a new line or move in to next
+    printf("%s is %d years old\n", name, age);
     return 0; // always last line of main
 }
