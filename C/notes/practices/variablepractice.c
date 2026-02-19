aae428221bcb5c55b5d38e2ec47bc00a62c6f09b
cimm3 <constanza.molina@ucas-edu.net>
2026-02-19 11:49:00 -0700

    variable practice

---
+++ C/notes/practices/variablepractice.c
@@ -0,0 +1,14 @@
+// CM P3, first progam C
+
+#include <stdio.h>
+
+int main(){}
+    char name[50];
+    const int lucky = 7;
+    const float decimal = 6.67; 
+    char breakfast[100];
+    char color[50];
+    char school_name[200];
+    int year = 2026;
+    int age[50];
+
