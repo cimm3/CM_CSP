5524f4bd2e93fa25e782610a57dfaf2d8478da79
cimm3 <constanza.molina@ucas-edu.net>
2026-02-19 10:57:23 -0700

    variables C

---
+++ C/notes/variables.c
@@ -0,0 +1,11 @@
+// This is a comment in C
+#include <stdio.h> // lets us create inputs and outputs 
+
+int main(){
+    int number = 12;
+    float pi = 3.14;
+
+    printf("%d", number);
+    printf("%f\n", pi); // \n tells computer to start a new line or move in to next
+    return 0; // always last line of main
+}
